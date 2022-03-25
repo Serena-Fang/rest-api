e53018bde064895fb10a9a3742b5c878cd0a15a6
Arpit Narechania <arpitnarechania@gatech.edu>
2022-03-25 07:49:50 -0400

    update gitignore; modify config

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ data_source = "json" # 'json' or 'mongodb'
 # Note: 2-D search only works with MongoDB and not a raw JSON file.
 raw_json_datafile = 'data/VitaLITy-1.0.0.json'
 
+# Topic Modelling datafiles
+raw_topicmodelling_json_datafile = 'data/getAllTopics.json'
+
 # if `data_source` = 'mongodb'
 mongodb_connection_uri = None
 mongodb_database = 'vissimilarity'
